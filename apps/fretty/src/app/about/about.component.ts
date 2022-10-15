1c074b4d7224c89c9a859b4bfa99fa03167cb68d
Lukas Schmitt <lukas.a.schmitt@gmail.com>
2022-10-15 16:07:22 +0200

    Rework about page

--- apps/fretty/src/app/about/about.component.ts
+++ apps/fretty/src/app/about/about.component.ts
@@ -5,17 +5,4 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent {
-  readonly licenses: Promise<string> | undefined;
-
-  constructor() {
-    this.licenses = fetch('3rdpartylicenses.txt')
-      .then((response) => {
-        if (response.ok) {
-          return response.text();
-        }
-        throw new Error();
-      })
-      .catch(() => 'Sorry, failed to load the third party licenses :(');
-  }
-}
+export class AboutComponent {}
--- apps/fretty/src/app/about/about.module.ts
+++ apps/fretty/src/app/about/about.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [AboutComponent],
-  imports: [CommonModule, MatDialogModule, MatExpansionModule, MatIconModule],
+  imports: [CommonModule, MatDialogModule, MatIconModule],
 })
 export class AboutModule {}
